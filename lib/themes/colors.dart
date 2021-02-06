acfb88f002ea169a8930fcb45cc381755c5379e4
Jacob Brasil <jnxtbrasil@gmail.com>
2021-02-06 13:56:00 -0500

    Added Theming

---
+++ lib/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static final Color black = Color.fromRGBO(32, 32, 32, 1.0);
+  static final Color white = Color.fromRGBO(255, 255, 255, 1.0);
+  static final Color lightGrey = Color.fromRGBO(0, 0, 0, 0.04);
+  static final Color transparent = Color.fromRGBO(0, 0, 0, 0.0);
+}
